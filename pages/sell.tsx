b20ebf78ee29b678c96fafbe6d6f77d409672a05
Alexandre Melle <alex.melle90@gmail.com>
2021-03-24 14:48:24 +0100

    Change sell from js to ts

--- pages/sell.tsx
+++ pages/sell.tsx
@@ -1,7 +1,7 @@
 import CreateProduct from '../components/CreateProduct';
 import PleaseSignIn from '../components/PleaseSignIn';
 
-export default function SellPage() {
+export default function SellPage(): JSX.Element {
   return (
     <div>
       <PleaseSignIn>
